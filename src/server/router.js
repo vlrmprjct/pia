fa4ac12e94a37b0e6c4d3b0eb4fdd97d05031417
Thomas M <mail@lokalnetz.com>
2021-01-09 10:23:42 +0100

    add mouser, oemsearch api express route

--- src/server/router.js
+++ src/server/router.js
@@ -23,6 +23,43 @@ apiRouter.get('/octopart/:query?', (req, res) => {
     }).pipe(res);
 });
 
+apiRouter.get('/oemsecret/:query?', (req, res) => {
+    request({
+        uri: 'https://beta.api.oemsecrets.com/partsearch',
+        qs: {
+            apiKey: process.env.OEMSECRET_API_KEY,
+            // currency: 'EUR',
+            // countryCode: 'DE',
+            // groupBy: 'distributor_name',
+            searchTerm: req.params.query
+        }
+    }).pipe(res);
+});
+
+
+apiRouter.get('/mouser/:query?', (req, res) => {
+    request({
+        uri: 'https://api.mouser.com/api/v1.0/search/keyword',
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            "SearchByKeywordRequest": {
+                "keyword": req.params.query,
+                "records": 0,
+                "startingRecord": 0,
+                // "searchOptions": "string",
+                // "searchWithYourSignUpLanguage": "string"
+            }
+        }),
+        qs: {
+            apikey: process.env.MOUSER_API_KEY
+        }
+    }).pipe(res);
+});
+
 apiRouter.get('/parts', (req, res) => {
     const entries = databaseClient.listEntries();
     res.status(200).send(entries);
