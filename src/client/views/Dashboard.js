08a168ae5905b11e686ff136db41bd18c04bc6f9
Thomas M <mail@lokalnetz.com>
2021-01-10 18:23:20 +0100

    cleanup dashboard view

--- src/client/views/Dashboard.js
+++ src/client/views/Dashboard.js
@@ -1,33 +1,11 @@
 import React from 'react';
 
-const Dashboard = (props) => {
+const Dashboard = () => {
     return (
-        <div className="uk-child-width-1-2@s uk-grid-match" uk-grid>
-            <div>
-                <div className="uk-card uk-card-hover uk-card-body">
-                    <h3 className="uk-card-title">Hover</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-            </div>
-            <div>
-                <div className="uk-card uk-card-default uk-card-hover uk-card-body">
-                    <h3 className="uk-card-title">Default</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-            </div>
-            <div>
-                <div className="uk-card uk-card-primary uk-card-hover uk-card-body uk-light">
-                    <h3 className="uk-card-title">Primary</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-            </div>
-            <div>
-                <div className="uk-card uk-card-secondary uk-card-hover uk-card-body uk-light">
-                    <h3 className="uk-card-title">Secondary</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                </div>
-            </div>
+        <div className="uk-margin uk-margin-left uk-margin-top">
+            Currently there is no useful Dashboard.
         </div>
+
     );
 };
 
