b6a7c76a5a58c7d3f9ee73dbe14fece957cb18ea
Thomas M <mail@lokalnetz.com>
2021-03-14 17:09:25 +0100

    remove obsolete api queries

--- src/server/router.js
+++ src/server/router.js
@@ -9,20 +9,6 @@ import databaseClientFactory from './databaseClient';
 const databaseClient = databaseClientFactory();
 const apiRouter = Router();
 
-apiRouter.get('/getUsername', (req, res) => {
-    res.send({ username: os.userInfo().username });
-});
-
-apiRouter.get('/octopart/:query?', (req, res) => {
-    request({
-        uri: 'https://octopart.com/api/v3/parts/search',
-        qs: {
-            apikey: process.env.OCTOPART_API_KEY,
-            q: req.params.query
-        }
-    }).pipe(res);
-});
-
 apiRouter.get('/oemsecret/:query?', (req, res) => {
     request({
         uri: 'https://beta.api.oemsecrets.com/partsearch',
@@ -70,14 +56,14 @@ apiRouter.get('/partcolumns', (req, res) => {
     res.status(200).send(entries);
 });
 
-apiRouter.get('/wikis/get/:id?', (req, res) => {
-    const entries = databaseClient.getEntry(req.params.id);
-    res.status(200).send(entries);
-});
-
 apiRouter.post('/part', (req, res) => {
     const updateEntry = databaseClient.updateEntry(req.body);
     res.status(200).send(updateEntry);
+});
+
+apiRouter.get('/part/:id?', (req, res) => {
+    const entries = databaseClient.getEntry(req.params.id);
+    res.status(200).send(entries);
 });
 
 apiRouter.post('/addpart', (req, res) => {
