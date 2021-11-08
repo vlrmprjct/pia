9ccd1c3a6924cd7b73740e9b6f785d3fb4553fcd
vlrmprjct <mail@lokalnetz.com>
2021-11-08 17:09:48 +0100

    [WIP]

--- src/server/index.js
+++ src/server/index.js
@@ -38,7 +38,10 @@ app.use(session({
     name: 'pia.sid',
     secret: process.env.SESSION_SECRET,
     resave: true,
-    saveUninitialized: true
+    rolling: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    // cookie: { maxAge: 5000 },
 }));
 
 app.use(express.urlencoded({ extended: true }));
@@ -54,8 +57,42 @@ app.use((req, res, next) => {
     next();
 });
 
+// app.use((req, res, next) => {
+//     //Checking previously set cookie (if there is one)
+//     const session = req.session.cookie._expires || '';
+//     console.log(session);
+//     console.log(new Date());
+
+//     console.log(session.getTime());
+//     console.log(new Date().getTime());
+
+
+//     // var d1 = new Date();
+//     // var d2 = new Date(d1);
+
+//     console.log(session.getTime() < new Date().getTime()); // prints true (correct)
+
+
+//     // res.redirect('/loginxx');
+//     if (session && new Date(session) < new Date()) {
+//         console.log('User session has expired.');
+//         // res.redirect('/login');
+//     }
+
+//     //Resetting the cookie
+//     // res.cookie('session', JSON.stringify({
+//     //     session: <sessionIDKeyHere>, expires: Date.now() + 3600000 }), {
+//     //         expires: new Date(2037, 0, 1),
+//     //         httpOnly: true,
+//     //         secure: true //Do you have https? If no, set to false
+//     // });
+
+//     next();
+// });
+
 app.use('/api', authRouter);
 app.use('/api', apiRouter);
+
 app.listen(process.env.PORT || 5000, () => {
     console.info(`Listening on port ${process.env.PORT || 5000}! 👾`);
 });
